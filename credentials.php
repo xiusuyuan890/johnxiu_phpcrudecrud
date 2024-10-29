f493a2b68ce33170da73bb1405ac4f8ee4c288a9
John Xiu <xiusuyuanis@gmail.com>
2024-10-29 01:45:46 +0000

    initial commit

---
+++ credentials.php
@@ -0,0 +1,7 @@
+<?php
+//Remember to populate the data here
+$servername = 'localhost';     //if apache on diff server from db or using Docker, set IP address
+$dbname = '';              //which db you're going to use
+$username = '';
+$password = '';
+?>
